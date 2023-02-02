03380103de82c3defa0b83760410736bb589f779
Xuan Ng <118996751+yeexng@users.noreply.github.com>
2023-02-02 01:37:28 +0000

    Change from Function to Class

--- epibooks/src/App.js
+++ epibooks/src/App.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
-import SingleBook from "./components/SingleBook";
 // import AllTheBooks from "./components/AllTheBooks";
 import BookList from "./components/BookList";
 import FantasyBooks from "./data/fantasy.json";
--- epibooks/src/components/BookList.jsx
+++ epibooks/src/components/BookList.jsx
@@ -1,24 +1,40 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Component } from 'react';
+import { Container, Row, Col, Form } from 'react-bootstrap';
 import SingleBook from './SingleBook';
 
-const BookList = (props) =>{
-    return(
-        <Container>
-            <Row className='justify-content-center'>
-                <h3 className='col-12 my-3'>Fantasy</h3>
-                {(props.listOfTheBooksAsProp).slice(0,12).map((singleBook)=>{
-                    return(
-                        <Col sm={10} md={5} lg={4} xl={3} key={singleBook.asin}>
-                            <SingleBook book={singleBook} />
-                            {/* singleBook value is extract from the map, therefore, should use singleBook, not props... */}
-                        </Col>
-                    )
-                })}
-                
-            </Row>
-        </Container>
+class BookList extends Component{
 
-    )
+
+
+    render(){
+        return(
+            <Container>
+                <Form onSubmit={this.filterBookList}>
+                    <Form.Control 
+                        // value={this.state.query} onChange={(e) => {
+                        // e.preventDefault();
+                        // console.log(e.target.value);
+                        // this.setState({ query: e.target.value });}} 
+                        type="text" placeholder="Search Here..."/>
+                </Form>
+
+                    <Row className='justify-content-center'>
+                    <h3 className='col-12 my-3'>Fantasy</h3>
+                    {(this.props.listOfTheBooksAsProp).slice(0,12).map((singleBook)=>{
+                        return(
+                            <Col sm={10} md={5} lg={4} xl={3} key={singleBook.asin}>
+                                <SingleBook book={singleBook} />
+                                {/* singleBook value is extract from the map, therefore, should use singleBook, not props... */}
+                            </Col>
+                            )
+                        })}
+                
+                    </Row>
+            </Container>
+
+        )
+    }
+
 
 }
 
